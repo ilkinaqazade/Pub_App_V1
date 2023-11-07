d157c9a94a86ea03f00012dc63d62731266ddcc9
ilkinaqazade <ilkinaqazade@gmail.com>
2023-11-07 16:11:08 +0400

    Add project files.

---
+++ Pub_App/Class/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pub_App.Class
+{
+    public class Product : BaseWiveModel
+    {
+        
+    }
+}
